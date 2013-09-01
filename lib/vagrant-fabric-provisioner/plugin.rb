5d20215c54881a712c84aec173af893a7dcab20f
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-02 08:03:56 +0900

    Add a locale file for the plugin.

--- lib/vagrant-fabric-provisioner/plugin.rb
+++ lib/vagrant-fabric-provisioner/plugin.rb
@@ -1,3 +1,6 @@
+# Add our custom translations to the load path
+I18n.load_path << File.expand_path("../../../locales/en.yml", __FILE__)
+
 module VagrantPlugins
   module Fabric
     class Plugin < Vagrant.plugin("2")
