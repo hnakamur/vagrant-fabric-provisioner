98446717033562e700ef270916c7186fcd5e6af2
callcc28 <closure28@gmail.com>
2013-12-15 16:13:30 +0900

    Fix identifier name bug

--- lib/vagrant-fabric/provisioner.rb
+++ lib/vagrant-fabric/provisioner.rb
@@ -6,7 +6,12 @@ module VagrantPlugins
         user = ssh_info[:username]
         host = ssh_info[:host]
         port = ssh_info[:port]
-        private_key = ssh_info[:private_key_path]
+        if ssh_info[:private_key_path].kind_of?(Array)
+          private_key = ssh_info[:private_key_path][0]
+        else
+          private_key = ssh_info[:private_key_path]
+        end
+
         if config.remote == false
           system "#{config.fabric_path} -f #{config.fabfile_path} " +
                 "-i #{private_key} --user=#{user} --hosts=#{host} " +
